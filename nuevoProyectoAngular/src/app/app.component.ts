4dba9834cde2b4a3720c19cdae1896203e040beb
JlFerrete <joseluisferrete@gmail.com>
2019-11-10 21:27:01 +0100

    Bindings y eventos

--- nuevoProyectoAngular/src/app/app.component.ts
+++ nuevoProyectoAngular/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'nuevoProyectoAngular';
+  constructor() {}
+
+  public saludar() {
+    alert("Hola Caracola");
+  }
+
+  title = "Mi primera app con Angular 7";
+  nombre = "Jose Luis";
+  apellido = "Ferrete";
 }
